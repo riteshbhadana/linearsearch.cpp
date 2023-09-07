9b79c659a1ac8c8f0560f7f0857ea93164e9523e
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-09-07 14:58:48 +0530

    Create linearsearch.cpp

---
+++ linearsearch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+ using namespace std;
+ bool search(int arr[], int size , int key){
+    for(int i=0; i<size; i++){
+        if(arr[i]==key){
+            return 1;
+        }
+    }
+    return 0;
+ }
+ int main () {
+    int arr [10] = {5,7,-2,10,22,-2,0,5,22,15};
+    cout <<"enter the element to searchfor" << endl;
+    int key;
+    bool found = search (arr,10,key);
+    if (found){
+        cout << "key is pesent" << endl;
+    }
+    else{
+        cout << "key is absent" << endl;
+    }
+    return 0; 
+
+ }
